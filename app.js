85772248bf361a9ececf450c709f9329a2e6c5dc
CharlesB13 <CharlesDavidButler@gmail.com>
2021-09-18 19:30:47 -0700

    Add Tilly and Platforms

--- app.js
+++ app.js
@@ -1,12 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
-    const tilly =document.createElement('div')
+    const tilly = document.createElement('div')
+    let tillyLeftSpace = 50
+    let startPoint = 150
+    let tillyBottomSpace = startPoint
+    let isGameOver = false
+    let platformCount = 5
+    let platforms = []
+   
 
-    function createTilly() {
-        grid.appendChild(tilly)
-        tilly.classList.add('tilly')
-        tilly.style.left = tillyLeftSpace + 'px'
-        doodler.style.bottom = doodlerBottomSpace + 'px'
+    class Platform {
+        constructor(newPlatBottom) {
+        this.bottom = newPlatBottom
+        this.left = Math.random() * 315
+        this.visual = document.createElement('div')
+
+        const visual = this.visual
+        visual.classList.add('platform')
+        visual.style.left = this.left + 'px'
+        visual.style.bottom = this.bottom + 'px'
+        grid.appendChild(visual)
     }
-    createTilly()
+    }
+
+    function createPlatforms() {
+        for (let i =0; i < platformCount; i++) {
+            let platGap = 600 / platformCount
+            let newPlatBottom = 100 + i * platGap
+            let newPlatform = new Platform (newPlatBottom)
+            platforms.push(newPlatform)
+        }
+    }
+    function createTilly() {
+            grid.appendChild(tilly)
+            tilly.classList.add('tilly')
+            tilly.style.left = tillyLeftSpace + 'px'
+            tilly.style.bottom = tillyBottomSpace + 'px'
+        
+    }
+
+    function start() {
+    if (!isGameOver) {
+        createTilly()
+        createPlatforms()
+    }
+ }
+start()
 })
